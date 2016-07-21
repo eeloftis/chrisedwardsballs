fd5499da6a2f443ce56fa3692f32f94189d3abe9
Ema Loftis <ema@Emas-MBP.hsd1.ma.comcast.net>
2016-07-21 00:25:14 -0400

    added footer

--- wp-content/themes/html5blank-stable/footer.php
+++ wp-content/themes/html5blank-stable/footer.php
@@ -1,18 +1,85 @@
+
+</div>
+<!-- /wrapper -->
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
+<section>
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
-				<!-- /copyright -->
 
+
+
+
+				<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#F2F2F2; clear:left; font:14px 'PT Sans', sans-serif; color: #524B48;}
+
+#mc_embed_signup h2 {
+	color: #060E9F;
+	text-transform: uppercase;
+	font-size: 22px;
+	 }
+
+	#mc-embedded-subscribe {
+
+		background-color: #060E9F !important;
+		color: #fff;
+
+		font-size: 24px;
+		text-transform: uppercase;
+		font-weight: bold !important;
+		border-radius: 2px;
+		box-shadow: 0 4px 2px -2px rgba(0,0,0,0.2);
+		font-family: 'PT Sans', sans-serif;
+		border-radius: 2px;
+
+
+	}
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="//chrisedwardsballs.us13.list-manage.com/subscribe/post?u=53a8b15f8bd074b8d14977b4e&amp;id=26f25db47e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<h2>Subscribe to BALLS</h2>
+<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+<div class="mc-field-group">
+	<label for="mce-FNAME">First Name </label>
+	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+</div>
+<div class="mc-field-group">
+	<label for="mce-LNAME">Last Name </label>
+	<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_53a8b15f8bd074b8d14977b4e_26f25db47e" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
+<!-- copyright -->
+<p class="copyright">
+	&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
+	<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
+</p>
+<!-- /copyright -->
+
+				<!-- <a class="synved-social-button synved-social-button-follow synved-social-size-32 synved-social-resolution-normal synved-social-provider-facebook nolightbox" data-provider="facebook" target="_blank" rel="nofollow" title="Follow us on Facebook" href="http://www.facebook.com/getballs" style="font-size: 0px; width:32px;height:32px;margin:0;margin-bottom:5px;margin-right:5px;"><img alt="facebook" title="Follow us on Facebook" class="synved-share-image synved-social-image synved-social-image-follow" width="32" height="32" style="display: inline; width:32px;height:32px; margin: 0; padding: 0; border: none; box-shadow: none;" src="http://www.chrisedwardsballs.com/wp-content/plugins/social-media-feather/synved-social/image/social/regular/32x32/facebook.png"></a><a  target="_blank" rel="nofollow" title="Follow us on Twitter" href="http://www.twitter.com/cedwardswrites" style="font-size: 0px; width:32px;height:32px;margin:0;margin-bottom:5px;margin-right:5px;"><img alt="twitter" title="Follow us on Twitter"  src="http://www.chrisedwardsballs.com/wp-content/plugins/social-media-feather/synved-social/image/social/regular/32x32/twitter.png"></a><a class="synved-social-button synved-social-button-follow synved-social-size-32 synved-social-resolution-normal synved-social-provider-linkedin nolightbox" data-provider="linkedin" target="_blank" rel="nofollow" title="Find us on Linkedin" href="http://www.linkedin.com/pub/chris-edwards/4/19/679" style="font-size: 0px; width:32px;height:32px;margin:0;margin-bottom:5px;"><img alt="linkedin" title="Find us on Linkedin" class="synved-share-image synved-social-image synved-social-image-follow" width="32" height="32" style="display: inline; width:32px;height:32px; margin: 0; padding: 0; border: none; box-shadow: none;" src="http://www.chrisedwardsballs.com/wp-content/plugins/social-media-feather/synved-social/image/social/regular/32x32/linkedin.png"></a> -->
+</section>
 			</footer>
 			<!-- /footer -->
 
-		</div>
-		<!-- /wrapper -->
 
 		<?php wp_footer(); ?>
 
--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -29,14 +29,14 @@
 
 
 	<!-- wrapper -->
-	<div class="wrapper head">
+	<div class="wrapper">
 			<!-- header -->
 			<header class="header clear" role="banner">
 
 					<!-- logo -->
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
-							
+
 							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" class="logo-img">
 						</a>
 					</div>
@@ -51,6 +51,6 @@
 			</header>
 			<!-- /header -->
 
-			</div>
+	</div>
 			<!-- wrapper -->
-			<div class="wrapper head">
+			<div class="wrapper">
