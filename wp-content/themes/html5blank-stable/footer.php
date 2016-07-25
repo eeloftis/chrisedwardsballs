56d1593f40557e5d1f47a60cc86fce129e0d770d
Ema Loftis <ema@Emas-MBP.hsd1.ma.comcast.net>
2016-07-25 18:16:11 -0400

    updated footer

--- wp-content/themes/html5blank-stable/footer.php
+++ wp-content/themes/html5blank-stable/footer.php
@@ -21,9 +21,9 @@
 						</div>
 
 				<ul class="footer-nav">
-					<li>Contact Chris</li>
-					<li>Pre-Order BALLS Now</li>
-					<li>Get BALLS E-News Alerts</li>
+					<li><a href="http://www.emilyloftis.com/chrisedwardsballs/contact/">Contact Chris</a></li>
+					<li><a href="http://www.emilyloftis.com/chrisedwardsballs/pre-order-now/">Pre-Order BALLS Now</a></li>
+					<li><a href="http://www.emilyloftis.com/chrisedwardsballs/get-balls-e-news-alerts/">Get BALLS E-News Alerts</a></li>
 				</ul>
 
 </div>
@@ -39,70 +39,6 @@
 
 <section>
 
-
-
-				<!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-	#mc_embed_signup{background:#FFF; clear:left; font:14px 'PT Sans', sans-serif; color: #000;}
-
-#mc_embed_signup h2 {
-	color: #000;
-
-	font-size: 22px;
-	 }
-
-	#mc-embedded-subscribe {
-
-		background-color: #060E9F !important;
-		color: #fff;
-
-		font-size: 24px;
-		text-transform: uppercase;
-		font-weight: bold !important;
-		border-radius: 2px;
-		box-shadow: 0 4px 2px -2px rgba(0,0,0,0.2);
-		font-family: 'PT Sans', sans-serif;
-		border-radius: 2px;
-
-
-	}
-
-	#mc-embedded-subscribe:hover {
-		text-decoration: underline;
-	}
-	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<!-- <div id="mc_embed_signup">
-<form action="//chrisedwardsballs.us13.list-manage.com/subscribe/post?u=53a8b15f8bd074b8d14977b4e&amp;id=26f25db47e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<h2>Subscribe to BALLS</h2>
-<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-<div class="mc-field-group">
-	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-</label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-</div>
-<div class="mc-field-group">
-	<label for="mce-FNAME">First Name </label>
-	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-</div>
-<div class="mc-field-group">
-	<label for="mce-LNAME">Last Name </label>
-	<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
-</div>
-	<div id="mce-responses" class="clear">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>     real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <!-- <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_53a8b15f8bd074b8d14977b4e_26f25db47e" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-</div>  -->
-
-<!--End mc_embed_signup-->
 
 
 
