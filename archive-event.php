a3f6168a82e8428940da92d293075bb2f231a4f3
Ema Loftis <ema@Emas-MacBook-Pro.local>
2016-07-31 00:01:38 -0400

    fixed archive page

--- archive-event.php
+++ archive-event.php
@@ -4,16 +4,27 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php _e( 'Archives', 'html5blank' ); ?></h1>
+			  <div class="border">
+
+					    <div class="content-block">
+
+			<h1><?php _e( 'Events', 'html5blank' ); ?></h1>
+
+
 
 			<?php get_template_part('loop'); ?>
 
 			<?php get_template_part('pagination'); ?>
 
+
+		</div>
+
+		</div>
+
 		</section>
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+
 
 <?php get_footer(); ?>
