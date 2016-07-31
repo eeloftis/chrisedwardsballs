633f4c77478c2d1a7c86932db63d29915388224c
Ema Loftis <ema@Emas-MacBook-Pro.local>
2016-07-30 20:32:14 -0400

    changed slider back

--- wp-content/themes/html5blank-stable/front-page.php
+++ wp-content/themes/html5blank-stable/front-page.php
@@ -11,7 +11,7 @@
   <div class="slider" id="here">
 
     <!-- category="2" for prod -->
- <?php echo do_shortcode('[slick-slider category="2" design="design-2"]'); ?>
+ <?php echo do_shortcode('[slick-slider category="2" design="design-1"]'); ?>
   </div>
 
 
