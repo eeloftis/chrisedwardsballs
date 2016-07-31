4eae1ff283c7709814dcaaa5dcef8c9a598dc3ab
Ema Loftis <ema@Emas-MacBook-Pro.local>
2016-07-30 20:26:50 -0400

    added events to front page

--- wp-content/themes/html5blank-stable/front-page.php
+++ wp-content/themes/html5blank-stable/front-page.php
@@ -11,7 +11,7 @@
   <div class="slider" id="here">
 
     <!-- category="2" for prod -->
- <?php echo do_shortcode('[slick-slider category="2" design="design-1"]'); ?>
+ <?php echo do_shortcode('[slick-slider category="2" design="design-2"]'); ?>
   </div>
 
 
@@ -20,14 +20,45 @@
   <div class="row clear">
     <div class="boxes"><h2>Upcoming Events</h2>
 
-        <div class="event">Carnegie Hall, New York, NY<br>
-          12.11.16</div>
 
-          <div class="event">XXX000, Boston, MA<br>
-            00.00.00</div>
 
-            <div class="event">XXX000, San Fransisco, CA<br>
-                00.00.00</div>
+<?php
+
+
+$args = array(
+    'post_type' => 'event',
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'front-page',
+
+        )
+    )
+);
+$query = new WP_Query( $args );
+
+
+//for a given post type, return all
+$event_post = 'event';
+
+
+    $args=array(
+      'post_type' => $event_post,
+      "front-page" => front-page,
+      'post_status' => 'publish'
+    );
+
+    $my_query = null;
+    $my_query = new WP_Query($args);
+  wp_reset_query();
+    if( $my_query->have_posts() ) {
+      while ($my_query->have_posts()) : $my_query->the_post(); ?>
+        <div class="event"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></div>
+        <?php
+      endwhile;
+    }
+      wp_reset_query();
+?>
+
 
     </div>
     <div class="boxes"><h2>Latest Press</h2>
--- wp-content/themes/html5blank-stable/functions.php
+++ wp-content/themes/html5blank-stable/functions.php
@@ -345,7 +345,7 @@ add_action('wp_print_scripts', 'html5blank_conditional_scripts'); // Add Conditi
 add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comments
 add_action('wp_enqueue_scripts', 'html5blank_styles'); // Add Theme Stylesheet
 add_action('init', 'register_html5_menu'); // Add HTML5 Blank Menu
-add_action('init', 'create_post_type_html5'); // Add our HTML5 Blank Custom Post Type
+add_action('init', 'create_events'); // Add our HTML5 Blank Custom Post Type
 add_action('widgets_init', 'my_remove_recent_comments_style'); // Remove inline Recent Comment Styles from wp_head()
 add_action('init', 'html5wp_pagination'); // Add our HTML5 Pagination
 
@@ -391,30 +391,27 @@ add_shortcode('html5_shortcode_demo_2', 'html5_shortcode_demo_2'); // Place [htm
 // Shortcodes above would be nested like this -
 // [html5_shortcode_demo] [html5_shortcode_demo_2] Here's the page title! [/html5_shortcode_demo_2] [/html5_shortcode_demo]
 
-/*------------------------------------*\
-	Custom Post Types
-\*------------------------------------*/
 
-// Create 1 Custom Post type for a Demo, called HTML5-Blank
-function create_post_type_html5()
+
+function create_events()
 {
-    register_taxonomy_for_object_type('category', 'html5-blank'); // Register Taxonomies for Category
-    register_taxonomy_for_object_type('post_tag', 'html5-blank');
-    register_post_type('html5-blank', // Register Custom Post Type
+    register_taxonomy_for_object_type('category', 'front-page'); // Register Taxonomies for Category
+    register_taxonomy_for_object_type('post_tag', 'event');
+    register_post_type('event', // Register Custom Post Type
         array(
         'labels' => array(
-            'name' => __('HTML5 Blank Custom Post', 'html5blank'), // Rename these to suit
-            'singular_name' => __('HTML5 Blank Custom Post', 'html5blank'),
-            'add_new' => __('Add New', 'html5blank'),
-            'add_new_item' => __('Add New HTML5 Blank Custom Post', 'html5blank'),
-            'edit' => __('Edit', 'html5blank'),
-            'edit_item' => __('Edit HTML5 Blank Custom Post', 'html5blank'),
-            'new_item' => __('New HTML5 Blank Custom Post', 'html5blank'),
-            'view' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'view_item' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'search_items' => __('Search HTML5 Blank Custom Post', 'html5blank'),
-            'not_found' => __('No HTML5 Blank Custom Posts found', 'html5blank'),
-            'not_found_in_trash' => __('No HTML5 Blank Custom Posts found in Trash', 'html5blank')
+            'name' => __('Events', 'events'), // Rename these to suit
+            'singular_name' => __('Event', 'event'),
+            'add_new' => __('Add New', 'event'),
+            'add_new_item' => __('Event', 'event'),
+            'edit' => __('Edit', 'event'),
+            'edit_item' => __('Edit Event', 'event'),
+            'new_item' => __('New Event', 'event'),
+            'view' => __('View HTML5 Blank Custom Post', 'event'),
+            'view_item' => __('View Event', 'event'),
+            'search_items' => __('Search Event', 'event'),
+            'not_found' => __('No Events found', 'event'),
+            'not_found_in_trash' => __('No Events found in Trash', 'event')
         ),
         'public' => true,
         'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
