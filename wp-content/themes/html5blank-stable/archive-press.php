cbf936bb3cff767da3541279ed3c01141d5af3d1
Ema Loftis <eloftis@wellesley.edu>
2016-09-26 01:23:19 -0400

    created press archive page

---
+++ wp-content/themes/html5blank-stable/archive-press.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+			  <div class="border">
+
+					    <div class="content-block">
+
+			<h1><?php _e( 'Events', 'html5blank' ); ?></h1>
+
+
+
+			<?php get_template_part('loop'); ?>
+
+			<?php get_template_part('pagination'); ?>
+
+
+		</div>
+
+		</div>
+
+		</section>
+		<!-- /section -->
+	</main>
+
+
+
+<?php get_footer(); ?>
