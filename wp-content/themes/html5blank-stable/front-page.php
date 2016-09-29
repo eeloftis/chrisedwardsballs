32d89fa4d82244acda6620b6418a8f47a92ed36d
Ema Loftis <eloftis@wellesley.edu>
2016-09-29 16:34:48 -0400

    added styling for press on front pagE

--- wp-content/themes/html5blank-stable/front-page.php
+++ wp-content/themes/html5blank-stable/front-page.php
@@ -68,7 +68,7 @@
 
       if( $press_query->have_posts() ) {
         while ($press_query->have_posts()) : $press_query->the_post(); ?>
-        <div><?php the_content(); ?></div>
+        <div class="press"><?php the_content(); ?></div>
           <?php
         endwhile;
       }
